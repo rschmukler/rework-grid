72c44036c43bbd2990b5b23341ea5ee0b6d964b6
Ryan Schmukler <ryan@womstreet.com>
2013-07-02 19:52:25 -0500

    Fix embarassing requires

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var mixins = require('rework-mixins');
 
 var grid = {};
 
-grid['grid-column'] = require('./lib/columns');
-grid['grid-position'] = require('./lib/position');
-grid['grid-container'] = require('./lib/container');
+grid['grid-column'] = require('./lib/columns.js');
+grid['grid-position'] = require('./lib/position.js');
+grid['grid-container'] = require('./lib/container.js');
 
 module.exports = function(_, rework) {
   rework.mixin(grid);
