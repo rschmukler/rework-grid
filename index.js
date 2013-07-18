9ffeb11f2e8e56dc3108d66debcf4253a4e5e3ec
Ryan Schmukler <ryan@womstreet.com>
2013-07-18 17:59:01 -0500

    Added column offset

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
   var grid = {
     'grid-column'   : require('./lib/columns.js')(config),
+    'grid-column-offset': require('./lib/column-offset.js')(config),
     'grid-position' : require('./lib/position.js'),
     'grid-container': require('./lib/container.js')(config)
   };
